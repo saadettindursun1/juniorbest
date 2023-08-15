f8e3b786e3c3b7055cedd7144d9f85e96d4359b9
Saadettin Dursun <58962578+saadettindursun1@users.noreply.github.com>
2023-08-15 17:16:33 +0300

    header ayrildi

---
+++ header.php
@@ -0,0 +1,10 @@
+
+        <nav class="navbar bg-transparent bg-body-tertiary pt-2">
+            <div class="container-fluid pl-5 pr-5">
+              <a class="navbar-brand"><img style="height: 100px; width: auto;" src="img/junior-best-logo-light.png" alt=""></a>
+              <div class="d-flex">
+                <button type="button" class="buttons-cerceve btn bg-transparent border-light text-light mr-3 pr-4 pl-4">Giriş Yap</button>
+                <button type="button" class="buttons-cerceve btn bg-transparent yesil-cerceve mr-3 pr-4 pl-4">Kayıt Ol</button>
+              </div>
+            </div>
+          </nav>
