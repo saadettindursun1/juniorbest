007b7f0f510b1e92fc06f73fbcfb69f558e894b8
Saadettin Dursun <58962578+saadettindursun1@users.noreply.github.com>
2023-08-18 00:26:17 +0300

    anasayfada görsel düzenlemeler yapıldı

--- header.php
+++ header.php
@@ -1,16 +1,14 @@
+<nav class="navbar sticky-top bg-transparent bg-body-tertiary pt-2">
+    <div class="container-fluid pl-5 pr-5">
+        <a class="navbar-brand"><img style="height: 100px; width: auto;" src="img/junior-best-logo-light.png"
+                alt=""></a>
+        <div class="d-flex">
+            <button type="button"
+                class="login-btn buttons-cerceve btn bg-transparent border-light text-light mr-3 pr-4 pl-4">Giriş
+                Yap</button>
+            <button type="button" class="signin-btn buttons-cerceve btn yesil-cerceve mr-3 pr-4 pl-4">Hemen Kayıt
+                Ol</button>
+        </div>
+    </div>
 
-        <nav class="navbar bg-transparent bg-body-tertiary pt-2">
-            <div class="container-fluid pl-5 pr-5">
-              <a class="navbar-brand"><img style="height: 100px; width: auto;" src="img/junior-best-logo-light.png" alt=""></a>
-              <div class="d-flex">
-                <button type="button" class="login-btn buttons-cerceve btn bg-transparent border-light text-light mr-3 pr-4 pl-4">Giriş Yap</button>
-                <button type="button" class="signin-btn buttons-cerceve btn bg-transparent yesil-cerceve mr-3 pr-4 pl-4">Kayıt Ol</button>
-              </div>
-            </div>
-            <div class="baslik-icerik col-md-6">
-              <h1>Haydi</h1>
-              <h2>Kodlayalım!</h2>
-              <p>Ekibini Kur, Yeteneğini Keşfet</p>
-              <a href="#" class="lets-start-button"><span>Hemen Başla</span></a>
-            </div>
-          </nav>
+</nav>
