150cc2c34919abb2bd911292a3a55cacc95afcc7
Saadettin Dursun <58962578+saadettindursun1@users.noreply.github.com>
2023-08-28 15:59:47 +0300

    birlestirildi

--- asamalar/adim-5.php
+++ asamalar/adim-5.php
@@ -10,26 +10,25 @@
         </div>
 
         <div class="form-group">
-            <input type="submit" value="Kaydet" name="desc-button" id="desc-button" class="btn btn-block btn-dark"
-                readonly>
+            <input type="submit" value="Kaydet" name="desc-button" id="desc-button" class="btn btn-block btn-dark" readonly>
         </div>
 
-        <?php 
-if (isset($_POST["desc-button"])) {
+        <?php
+        if (isset($_POST["desc-button"])) {
 
-    @$desc = $_POST["desc"];
-    @$short_desc = $_POST["short-desc"];
+            @$desc = $_POST["desc"];
+            @$short_desc = $_POST["short-desc"];
 
-    $data->bio->desc = $desc;
-    $data->bio->short_desc = $short_desc;
-    $data->asama = "6";
-    
-    $json_data_update = json_encode($data, JSON_UNESCAPED_UNICODE);
-    $tag_query = "user_info='" . $json_data_update . "' , user_login_type='2'";
-    $tag_where = "user_mail='" . $code_mail . "'";
-    $jb_mysql->update("users", $tag_query, $tag_where);
-    header("Refresh:0;Url=panel/anasayfa.php");
-}
+            $data->bio->desc = $desc;
+            $data->bio->short_desc = $short_desc;
+            $data->asama = "6";
+
+            $json_data_update = json_encode($data, JSON_UNESCAPED_UNICODE);
+            $tag_query = "user_info='" . $json_data_update . "' , user_login_type='2'";
+            $tag_where = "user_mail='" . $code_mail . "'";
+            $jb_mysql->update("users", $tag_query, $tag_where);
+            header("Refresh:0;Url=panel/anasayfa.php");
+        }
         ?>
     </div>
 </form>
